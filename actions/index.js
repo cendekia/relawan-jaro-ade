03651fc2f134bfa0f8211859f6b30ea68a403927
Cendekia Pramana Putra <me@cendekiapp.com>
2017-10-13 17:43:23 +0700

    set action for volunteer registration form

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,94 @@
-import { NEW_REGISTER, FB_LOGGED_IN } from '../constants'
+import {
+  FB_LOGGED_IN,
+  SET_VOLUNTEER_VILLAGE,
+  SET_VOLUNTEER_DISTRICT,
+  SET_VOLUNTEER_DAPIL,
+  SET_VOLUNTEER_NAME,
+  SET_VOLUNTEER_DOB,
+  SET_VOLUNTEER_ADDRESS,
+  SET_VOLUNTEER_NO_KTP,
+  SET_VOLUNTEER_NO_HP,
+  SET_VOLUNTEER_NO_WA,
+  SET_VOLUNTEER_PHOTO,
+  SET_VOLUNTEER_PHOTO_KTP,
+} from '../constants'
 
-export function newRegister(data) {
+
+export function setVolunteerVillage(village) {
   return {
-    type: NEW_REGISTER,
-    ...data
+    type: SET_VOLUNTEER_VILLAGE,
+    village
+  }
+}
+
+export function setVolunteerDistrict(district) {
+  return {
+    type: SET_VOLUNTEER_DISTRICT,
+    district
+  }
+}
+
+export function setVolunteerDapil(dapil) {
+  return {
+    type: SET_VOLUNTEER_DAPIL,
+    dapil
+  }
+}
+
+export function setVolunteerName(name) {
+  return {
+    type: SET_VOLUNTEER_NAME,
+    name
+  }
+}
+
+
+export function setVolunteerDob(dob) {
+  return {
+    type: SET_VOLUNTEER_DOB,
+    dob
+  }
+}
+
+export function setVolunteerAddress(address) {
+  return {
+    type: SET_VOLUNTEER_ADDRESS,
+    address
+  }
+}
+
+export function setVolunteerNoKTP(idNumber) {
+  return {
+    type: SET_VOLUNTEER_NO_KTP,
+    idNumber
+  }
+}
+
+export function setVolunteerNoHP(phoneNumber) {
+  return {
+    type: SET_VOLUNTEER_NO_HP,
+    phoneNumber
+  }
+}
+
+export function setVolunteerNoWA(waNumber) {
+  return {
+    type: SET_VOLUNTEER_NO_WA,
+    waNumber
+  }
+}
+
+export function setVolunteerPhoto(photo) {
+  return {
+    type: SET_VOLUNTEER_PHOTO,
+    photo
+  }
+}
+
+export function setVolunteerPhotoKTP(photoKTP) {
+  return {
+    type: SET_VOLUNTEER_PHOTO_KTP,
+    photoKTP
   }
 }
 
