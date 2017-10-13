c349d627469005b4f8a8361af2f6ed18b720a762
Cendekia Pramana Putra <me@cendekiapp.com>
2017-10-13 17:43:03 +0700

    set reducers for volunteer registration form

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,32 @@
 import { combineReducers } from 'redux'
-import { NEW_REGISTER, FB_LOGGED_IN } from '../constants'
+import {
+  FB_LOGGED_IN,
+  SET_VOLUNTEER_VILLAGE,
+  SET_VOLUNTEER_DISTRICT,
+  SET_VOLUNTEER_DAPIL,
+  SET_VOLUNTEER_NAME,
+  SET_VOLUNTEER_DOB,
+  SET_VOLUNTEER_ADDRESS,
+  SET_VOLUNTEER_NO_KTP,
+  SET_VOLUNTEER_NO_HP,
+  SET_VOLUNTEER_NO_WA,
+  SET_VOLUNTEER_PHOTO,
+  SET_VOLUNTEER_PHOTO_KTP,
+} from '../constants'
+
+const defaultState = {
+  village: undefined,
+  district: undefined,
+  dapil: undefined,
+  name: undefined,
+  dob: undefined,
+  address: undefined,
+  idNumber: undefined,
+  phoneNumber: undefined,
+  waNumber: undefined,
+  photo: undefined,
+  photoKTP: undefined,
+}
 
 const logIn = (state = {}, action) => {
   switch (action.type) {
@@ -15,23 +42,74 @@ const logIn = (state = {}, action) => {
   }
 }
 
-const addNewRegister = (state = {}, action) => {
+const setVolunteerForm = (state = defaultState, action) => {
   switch (action.type) {
-    case NEW_REGISTER:
+    case SET_VOLUNTEER_VILLAGE:
       return {
         ...state,
         village: action.village,
-        district: action.district,
-        dapil: action.dapil,
-        name: action.name,
-        dob: action.dob,
-        address: action.address,
-        idCard: action.idCard,
-        mobilePhone: action.mobilePhone,
-        status: action.status,
-        idCardImage: action.idCardImage,
-        profilePicture: action.profilePicture,
       }
+      break
+    case SET_VOLUNTEER_DISTRICT:
+      return {
+        ...state,
+        district: action.district,
+      }
+      break
+    case SET_VOLUNTEER_DAPIL:
+      return {
+        ...state,
+        dapil: action.dapil,
+      }
+      break
+    case SET_VOLUNTEER_NAME:
+      return {
+        ...state,
+        name: action.name,
+      }
+      break
+    case SET_VOLUNTEER_DOB:
+      return {
+        ...state,
+        dob: action.dob,
+      }
+      break
+    case SET_VOLUNTEER_ADDRESS:
+      return {
+        ...state,
+        address: action.address,
+      }
+      break
+    case SET_VOLUNTEER_NO_KTP:
+      return {
+        ...state,
+        idNumber: action.idNumber,
+      }
+      break
+    case SET_VOLUNTEER_NO_HP:
+      return {
+        ...state,
+        phoneNumber: action.phoneNumber,
+      }
+      break
+    case SET_VOLUNTEER_NO_WA:
+      return {
+        ...state,
+        waNumber: action.waNumber,
+      }
+      break
+    case SET_VOLUNTEER_PHOTO:
+      return {
+        ...state,
+        photo: action.photo,
+      }
+      break
+    case SET_VOLUNTEER_PHOTO_KTP:
+      return {
+        ...state,
+        photoKTP: action.photoKTP,
+      }
+      break
     default:
       return state
   }
@@ -41,6 +119,6 @@ export default function getRootReducer(navReducer) {
     return combineReducers({
         nav: navReducer,
         loginResponse: logIn,
-        newRegister: addNewRegister
+        volunteerForm: setVolunteerForm
     });
 }
