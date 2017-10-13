11c359d42b2581a2a8db51b8eef66707e7f1eabe
Cendekia Pramana Putra <me@cendekiapp.com>
2017-10-13 17:42:02 +0700

    set contants for volunteer form

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,17 @@ import { Constants } from 'expo';
 
 export const STATUS_BAR_HEIGHT = Constants.statusBarHeight;
 
-// Redux constants
-export const NEW_REGISTER = 'NEW_REGISTER'
 export const FB_LOGGED_IN = 'FB_LOGGED_IN'
+
+// registration form constants
+export const SET_VOLUNTEER_VILLAGE = 'SET_VOLUNTEER_VILLAGE'
+export const SET_VOLUNTEER_DISTRICT = 'SET_VOLUNTEER_DISTRICT'
+export const SET_VOLUNTEER_DAPIL = 'SET_VOLUNTEER_DAPIL'
+export const SET_VOLUNTEER_NAME = 'SET_VOLUNTEER_NAME'
+export const SET_VOLUNTEER_DOB = 'SET_VOLUNTEER_DOB'
+export const SET_VOLUNTEER_ADDRESS = 'SET_VOLUNTEER_ADDRESS'
+export const SET_VOLUNTEER_NO_KTP = 'SET_VOLUNTEER_NO_KTP'
+export const SET_VOLUNTEER_NO_HP = 'SET_VOLUNTEER_NO_HP'
+export const SET_VOLUNTEER_NO_WA = 'SET_VOLUNTEER_NO_WA'
+export const SET_VOLUNTEER_PHOTO = 'SET_VOLUNTEER_PHOTO'
+export const SET_VOLUNTEER_PHOTO_KTP = 'SET_VOLUNTEER_PHOTO_KTP'
